195b2cab8a85a81dfddfa05c24d6aa86c1a5fb27
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-11-26 23:20:31 +0100

    fixed scripts

--- initalize/initial_install_script.sh
+++ initalize/initial_install_script.sh
@@ -4,6 +4,8 @@ apt-get update
 apt-get install nodejs npm
 apt-get install libopenzwave-doc libopenzwave1.5 libopenzwave1.5-dev
 cp ./init_zwave.service /etc/systemd/system/init_zwave.service
-systemctl start init_zwave.service 
+systemctl daemon-reload
+systemctl enable init_zwave.service
+systemctl restart init_zwave.service
 (crontab -l 2>/dev/null; echo "30 23 * * * home/pi/Raspberry_Sensor_Hub/initialize/update_script.sh") | crontab -
 
--- initalize/update_script.sh
+++ initalize/update_script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "update started"
 echo "Stopping service"
-stop init_zwave
+systemctl stop init_zwave.service
 echo "stopped service"
 cd "~/Rasperry_Sensor_Hub"
 git pull https://github.com/GF3R/Rasperry_Sensor_Hub.git
 echo "restarting service"
-start init_zwave
+systemctl start init_zwave.service
 
