88e90e80bbfcc887c71e01875818360490b3bc1d
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-11-26 23:45:32 +0100

    fixed install script

--- initalize/initial_install_script.sh
+++ initalize/initial_install_script.sh
@@ -3,10 +3,12 @@ echo "installing necessary libs and software"
 apt-get update
 apt-get install nodejs npm
 apt-get install libopenzwave-doc libopenzwave1.5 libopenzwave1.5-dev
+echo "setting up service"
 cp ./init_zwave.service /etc/systemd/system/init_zwave.service
 systemctl daemon-reload
 systemctl enable init_zwave.service
 systemctl restart init_zwave.service
+echo "setting up regular updates"
 chmod +x update_script.sh
-(crontab -l 2>/dev/null; echo "30 23 * * * home/pi/Raspberry_Sensor_Hub/initialize/update_script.sh > /home/pi/cron_exec.log") | crontab -
+(crontab -l 2>/dev/null; echo "30 23 * * * home/pi/Raspberry_Sensor_Hub/initialize/update_script.sh") | crontab -
 
