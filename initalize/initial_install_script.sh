9ea0495e102ce17204f6d14bc89c63122593ffe1
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-11-26 23:48:54 +0100

    added sudo to crontab

--- initalize/initial_install_script.sh
+++ initalize/initial_install_script.sh
@@ -8,7 +8,7 @@ cp ./init_zwave.service /etc/systemd/system/init_zwave.service
 systemctl daemon-reload
 systemctl enable init_zwave.service
 systemctl restart init_zwave.service
-echo "setting up regular updates"
+echo "setting up regular"
 chmod +x update_script.sh
-(crontab -l 2>/dev/null; echo "30 23 * * * home/pi/Raspberry_Sensor_Hub/initialize/update_script.sh") | crontab -
+(crontab -l 2>/dev/null; echo "30 23 * * * sudo home/pi/Raspberry_Sensor_Hub/initialize/update_script.sh") | crontab -
 
