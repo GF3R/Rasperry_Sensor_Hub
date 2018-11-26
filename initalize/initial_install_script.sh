e1f7c97dd62ad80b7d937131b98596193094fffb
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-11-26 16:57:39 +0100

    update init script

--- initalize/initial_install_script.sh
+++ initalize/initial_install_script.sh
@@ -7,4 +7,5 @@ git clone https://github.com/GF3R/Rasperry_Sensor_Hub.git
 cd Rasperry_Sensor_Hub
 cp ./init_zwave.conf /etc/init/ 
 start init_zwave
+(crontab -l 2>/dev/null; echo "30 23 * * * ~/Raspberry_Sensor_Hub/initialize/update_script.sh") | crontab -
 
