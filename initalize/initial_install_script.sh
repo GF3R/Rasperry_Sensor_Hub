12a9285f933e77dbc8de5f1ee30a149b8e306af6
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-11-26 22:54:54 +0100

    added tmux

--- initalize/initial_install_script.sh
+++ initalize/initial_install_script.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 echo "installing necessary libs and software"
-update
-apt-get install npm git 
+apt-get update
+apt-get install nodejs npm tmux
 apt-get install libopenzwave-doc libopenzwave1.5 libopenzwave1.5-dev
-git clone https://github.com/GF3R/Rasperry_Sensor_Hub.git
-cd Rasperry_Sensor_Hub
+cd ../Rasperry_Sensor_Hub
 cp ./init_zwave.conf /etc/init/ 
 start init_zwave
 (crontab -l 2>/dev/null; echo "30 23 * * * ~/Raspberry_Sensor_Hub/initialize/update_script.sh") | crontab -
