06bf889bdabfdca14d8378114822513555a57cfb
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-11-27 13:55:50 +0100

    added option to enable service

--- initalize/initial_install_script.sh
+++ initalize/initial_install_script.sh
@@ -3,12 +3,14 @@ echo "installing necessary libs and software"
 apt-get update
 apt-get install nodejs npm
 apt-get install libopenzwave-doc libopenzwave1.5 libopenzwave1.5-dev
-echo "setting up service"
-cp ./init_zwave.service /etc/systemd/system/init_zwave.service
-systemctl daemon-reload
-systemctl enable init_zwave.service
-systemctl restart init_zwave.service
-echo "setting up regular"
+if [ "$1" = "-s" ]; then
+  echo "setting up service"
+  cp ./init_zwave.service /etc/systemd/system/init_zwave.service
+  systemctl daemon-reload
+  systemctl enable init_zwave.service
+  systemctl restart init_zwave.service
+  echo "Service was initialized"
+fi
+echo "setting up regular updates"
 chmod +x update_script.sh
 (crontab -l 2>/dev/null; echo "30 23 * * * sudo home/pi/Raspberry_Sensor_Hub/initialize/update_script.sh") | crontab -
-
