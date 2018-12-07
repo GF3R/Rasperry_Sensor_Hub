c5f1e99200ee1936e5c821daa99b1538d672dda1
Lukas Hofer <lukashofer@bluemail.ch>
2018-12-07 14:18:52 +0100

    added zwave nodes

--- src/zwave_devices/switch_binary.js
+++ src/zwave_devices/switch_binary.js
@@ -6,6 +6,39 @@ zwave.on('notification', function(nodeid, notif) {
 	console.log('message %d for node %d', notif, nodeid);
 });
 
+zwave.on('node ready', function(nodeid, nodeinfo) {
+  nodes[nodeid]['manufacturer'] = nodeinfo.manufacturer;
+  nodes[nodeid]['manufacturerid'] = nodeinfo.manufacturerid;
+  nodes[nodeid]['product'] = nodeinfo.product;
+  nodes[nodeid]['producttype'] = nodeinfo.producttype;
+  nodes[nodeid]['productid'] = nodeinfo.productid;
+  nodes[nodeid]['type'] = nodeinfo.type;
+  nodes[nodeid]['name'] = nodeinfo.name;
+  nodes[nodeid]['loc'] = nodeinfo.loc;
+  nodes[nodeid]['ready'] = true;
+  console.log('node%d: %s, %s', nodeid,
+    nodeinfo.manufacturer ? nodeinfo.manufacturer : 'id=' + nodeinfo.manufacturerid,
+    nodeinfo.product ? nodeinfo.product : 'product=' + nodeinfo.productid +
+    ', type=' + nodeinfo.producttype);
+  console.log('node%d: name="%s", type="%s", location="%s"', nodeid,
+    nodeinfo.name,
+    nodeinfo.type,
+    nodeinfo.loc);
+  for (comclass in nodes[nodeid]['classes']) {
+    switch (comclass) {
+      case 0x25: // COMMAND_CLASS_SWITCH_BINARY
+      case 0x26: // COMMAND_CLASS_SWITCH_MULTILEVEL
+        zwave.enablePoll(nodeid, comclass);
+        break;
+    }
+    var values = nodes[nodeid]['classes'][comclass];
+    console.log('node%d: class %d', nodeid, comclass);
+    for (idx in values)
+      console.log('node%d:   %s=%s', nodeid, values[idx]['label'], values[
+        idx]['value']);
+  }
+});
+
 zwave.connect(driverPath);
 console.log("connected");
 zwave.setValue(2,37,1,0,true);
