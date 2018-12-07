3975fa87d8c3427f097edb91b369b201a7b03a55
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-12-07 14:35:23 +0100

    added push

---
+++ src/main.js
@@ -0,0 +1,16 @@
+var DiscoveryStick = require("./zwave_devices/discovery_stick");
+var discoveryStick = DiscoveryStick('/dev/serial/by-id/usb-0658_0200-if00');
+var deviceList = [];
+
+discoveryStick.addDeviceListener(function(nodeid, comclass, valueId){
+  searchStr = valueId.split('-').splice(1).join('-')
+  switch(searchStr){
+    case "37-1-0": 
+      deviceList[nodeid] = new SwitchBinary(nodeid, discoveryStick.getZwave());
+      break;
+    case "case": 
+      break;
+    case "case": 
+      break;
+  }
+});
---
+++ src/zwave_devices/discovery_stick.js
@@ -0,0 +1,19 @@
+class DiscoveryStick {
+  constructor(path) {
+    var driverPath = path;
+    var ZWave = require(path);
+    var zwave = new ZWave({
+      ConsoleOutput: false
+    });
+    zwave.connect(driverPath);
+  }
+
+  getZwave() {
+    return zwave;
+  }
+
+  addDeviceListener(listener){
+    zwave.on('value added', listener);
+  }
+
+}
