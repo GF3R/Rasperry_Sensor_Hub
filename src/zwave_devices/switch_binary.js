c9eb8bb4f80926f22036416a7fc8e393aa32f223
Lukas Hofer <lukashofer@bluemail.ch>
2018-12-07 14:30:43 +0100

    bugfixes

--- src/zwave_devices/switch_binary.js
+++ src/zwave_devices/switch_binary.js
@@ -1,6 +1,6 @@
 var ZWave = require('../node_modules/openzwave-shared/lib/openzwave-shared.js');
 var zwave = new ZWave({ConsoleOutput: false});
-var driverPath = '/dev/serial/by-id/usb-0658_0200_if00';
+var driverPath = '/dev/serial/by-id/usb-0658_0200-if00';
 
 zwave.on('notification', function(nodeid, notif) {
 	console.log('message %d for node %d', notif, nodeid);
