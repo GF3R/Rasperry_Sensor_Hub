d56e901d43991bd905e0a6595c786c28dff7dcc8
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-12-21 10:41:29 +0100

    deviceuuid

--- src/base_devices/device.js
+++ src/base_devices/device.js
@@ -1,16 +1,16 @@
-const ZWave = require('../node_modules/mqtt/mqtt.js');
+const mqtt = require('../node_modules/mqtt/mqtt.js');
 
 class Device {
 
     constructor() {
+
         this.topicbase = "nexhome/";
         this.brokerUrl = "mqtt://broker.lab.nexhome.ch";
-        this.mqtt = require('mqtt');
         var options = {
             username: 'superiot',
             password: new Buffer('strPassword')
         };
-        this.mqttClient = this.mqtt.connect(this.brokerUrl, options);
+        this.mqttClient = mqtt.connect(this.brokerUrl, options);
         //Init Pub and Sub topic
         this.deviceUuid = "dummyid"; //TODO get some sort of id? maybe from zwave?
         this.pub_topic = this.topicbase + "data/" + this.deviceUuid;
