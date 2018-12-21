c4693d86b1cb98912095cd6916798b7699591beb
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-12-21 14:19:46 +0100

    what

--- src/base_devices/device.js
+++ src/base_devices/device.js
@@ -4,7 +4,7 @@ class Device {
         logger.trace("constructing device");
         //Init Pub and Sub topic
         this.topicbase = "nexhome/";
-        this.deviceUuid = "87bcf1d5-968e-44c9-a536-896b3f792b" + deviceUuid; //TODO get some sort of id? maybe from zwave?
+        this.deviceUuid = "87bcf1d5-968e-44c9-a536-896b3f792b4" + deviceUuid; //TODO get some sort of id? maybe from zwave?
 	    console.log(this.deviceUuid);
         this.pub_topic = this.topicbase + "data/" + this.deviceUuid;
         this.sub_topic = this.topicbase + "event/" + this.deviceUuid;
